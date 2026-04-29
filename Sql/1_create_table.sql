77b1f334b93f730e45f2e45207ad3b7abdbcb7f8
Michele Soraya <38866795+miihsoraya@users.noreply.github.com>
2026-04-29 13:46:55 -0300

    Create department table with dept_name and building
    Define the structure for the department table.

--- Sql/1_create_table.sql
+++ Sql/1_create_table.sql
@@ -1,13 +1,3 @@
--- CREATE TABLE r 
--- (A_1 D_1,
--- A_2 D_2,
--- ..
--- A_n D_n,
--- <integrity_restriction_1>
--- .. 
--- <integrity_restriction_k>)
-
-
 CREATE TABLE
     department (
         dept_name varchar(20),
