20f3f9c4cd876a875c087363e049991101df9755
Michele Soraya <38866795+miihsoraya@users.noreply.github.com>
2026-04-29 12:37:46 -0300

    Fix formatting of SQL insert values

--- Sql/Insert_Values.sql
+++ Sql/Insert_Values.sql
@@ -1,9 +1,9 @@
 INSERT INTO
   department
 VALUES
-  ('Informatica', 'Pombal', 700000),
+  	('Informatica', 'Pombal', 700000),
 	('Biologia', 'Sociais', 900000),
-  ('Teatro', 'Humanas', 500000),
+  	('Teatro', 'Humanas', 500000),
 	('Mat laboratoriais', 'Farmacia', 950000),
 	('Eng aeroespacial', 'Paulo Freire', 1000000),
 	('Informatica_2', 'Pombal', 600000),
